e34b522220bb8da361051f1ae2608999b298dabc
Haoran-Chen-1115 <115064685+Haoran-Chen-1115@users.noreply.github.com>
2022-10-05 15:58:20 +0800

    Add files via upload

--- main_nscf/0_Public/parameter.m
+++ main_nscf/0_Public/parameter.m
@@ -13,16 +13,16 @@ L_OVERL_OLD=false; % should not be changed
 
 %% Simulation parameters
 ROOT_DIR='../../'; % Directory where PIMD is performed
-TEMP=450; % Temperature
-NCONF=10000; % Configuration number
-NSTART=2000; % Pre-equilibrated steps
-NBEAD=1; % Bead number (Trotter number)
+TEMP=190; % Temperature
+NCONF=7000; % Configuration number
+NSTART=1000; % Pre-equilibrated steps
+NBEAD=16; % Bead number (Trotter number)
 NSKIP=5; % Number of steps between calculations
 
-ENCUT=300; % Plane-wave energy cutoff
+ENCUT=450; % Plane-wave energy cutoff
 
 %% Some information for calculating the Green's function
-NELE_TYP=[6,11]; % valance electrons for each type
+NELE_TYP=[1,6]; % valance electrons for each type
 NBANDS_mul = 3^3; % Bands number to be included: NBANDS=NBANDS_mul*NELE/2
 
 LBloch = false; % Whether Hamiltionians are expanded in terms of initial Bloch waves
@@ -32,8 +32,8 @@ KOMEGA=16; % omega_Ns in quasi-static approximation
 %% Supercell shape and k-mesh
 N_sc_base = [2 2 2]; % Base 'primitive' cell for constructing non-diagonal supercell
 N_sc_ex = [1 0 0; 0 1 0; 0 0 1]; % Non-diagonal supercell shape
-N_k = [2,2,2]; % of the 2*2*2 supercell, self-consistent k-mesh
-N_k_nscf = [4,4,4]; % of the 2*2*2 supercell, non-self-consistent k-mesh
+N_k = [6,6,6]; % of the 2*2*2 supercell, self-consistent k-mesh
+N_k_nscf = [12,12,12]; % of the 2*2*2 supercell, non-self-consistent k-mesh
 
 %% Morel-Anderson Pseudopotential
 mustar = 0.12;
