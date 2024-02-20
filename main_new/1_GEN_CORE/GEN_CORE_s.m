4bae0b211a38d8c17ee726c40b0f8f8ece7e7782
Haoran-Chen-1115 <115064685+Haoran-Chen-1115@users.noreply.github.com>
2024-02-20 13:49:17 +0800

    Add files via upload

--- main_new/1_GEN_CORE/GEN_CORE_s.m
+++ main_new/1_GEN_CORE/GEN_CORE_s.m
@@ -6,15 +6,6 @@ addpath('../0_Public')
 parpool(10,'IdleTimeout', 2880)
 
 %% Pseudo basis we care about
-% Read in information about grid and the index for calculated G vectors
-% It should be the same for all beads
-FID=fopen('../../BEAD_1/INDEX_1_1_1');
-NGX=fread(FID,1,'int');NGY=fread(FID,1,'int');NGZ=fread(FID,1,'int');
-NPL=fread(FID,1,'int');
-G_INDEX=fread(FID,[4,NPL],'int');
-fclose(FID);
-
-NPLWV=NGX*NGY*NGZ;
 % GREEN=complex(zeros(NPL,NPL,'single','gpuArray'));
 % GREEN=zeros(NPL,NPL,'gpuArray')+1i*zeros(NPL,NPL,'gpuArray');
 
@@ -35,19 +26,9 @@ OMEGA=det(A);
 % Force NKPTS=1
 NKPTS=1;
 
-% B=gpuArray(B);G_INDEX=gpuArray(G_INDEX);
-% G=zeros(4,NPL,'gpuArray');
-B=gpuArray(B);G_INDEX=gpuArray(G_INDEX);
-G=zeros(4,NPL,'gpuArray');
-G(1:3,:)=B.'*G_INDEX(1:3,:);
-G(4,:)=G(1,:).^2+G(2,:).^2+G(3,:).^2;
-
-clear G2
-G2(4,:)=sqrt(G(4,:));
-G2(4,:)=max(G2(4,:),1E-12);
-G2(1:3,:)=bsxfun(@rdivide,G(1:3,:),G2(4,:));
-
-G=single(G);
+% Read in information about grid and the index for calculated G vectors
+% It should be the same for all beads
+[G_INDEX,G,G2,NPL,NPLWV,NGX,NGY,NGZ]=RD_INDEX(ROOT_DIR,'1','1',[0;0;0],B,ENCUT)
 
 %% Calculate T_core
 % Information obtained from POTCAR, which is the same for all configuartions
